7bb1889cb58bb616990ed19ac10a2daa1433fdb1
e.ghalibaf <erfanwmb@gmail.com>
2023-12-25 12:46:06 +0330

    write facade to chose version laravel and call rule

--- src/CaptchaFacade.php
+++ src/CaptchaFacade.php
@@ -3,12 +3,31 @@
 namespace erfanwmb\captcha;
 
 use Illuminate\Support\Facades\Facade;
+use erfanwmb\captcha\app\Rules\Captcha\V10\CaptchaValidator as CaptchaValidator10;
+use erfanwmb\captcha\app\Rules\Captcha\V8\CaptchaValidator as CaptchaValidator8;
+
 
 class CaptchaFacade extends Facade{
+
+    public function __construct()
+    {
+
+    }
+
     protected static function getFacadeAccessor()
     {
         return 'eCaptcha';
     }
 
+    public static function validate(string $exclusive_captcha=null)
+    {
+
+        $laravel_version=explode('.',app()::VERSION)[0];
+        switch ($laravel_version){
+            case 8 : return new CaptchaValidator8($exclusive_captcha);
+            case 9:
+            case 10 : return new CaptchaValidator10($exclusive_captcha);
+        }
+    }
 
 }
