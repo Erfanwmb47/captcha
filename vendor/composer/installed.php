fccc9d62fc304a1c37e6b2f709231745296b85f6
e.ghalibaf <erfanwmb@gmail.com>
2024-01-24 12:29:07 +0330

    set autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'erfanwmb/captcha',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd834731a450af44cf3b0b926371cf5b01969dc0e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'erfanwmb/captcha' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd834731a450af44cf3b0b926371cf5b01969dc0e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
